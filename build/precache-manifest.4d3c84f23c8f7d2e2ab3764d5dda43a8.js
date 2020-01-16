6e7ba7c6e08335e2128d12ac7e1f43fbdd9ccddb
AJ Esamann <aje0098@gmail.com>
2020-01-16 14:54:54 -0500

    fixed css error

---
+++ build/precache-manifest.4d3c84f23c8f7d2e2ab3764d5dda43a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfaa8b53941bb6e38f60a58f893007a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be5897d1c11b905ad84b",
+    "url": "/static/css/main.b707aaf7.chunk.css"
+  },
+  {
+    "revision": "0973360b12121e5c4b44",
+    "url": "/static/js/2.6ff97f2a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6ff97f2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be5897d1c11b905ad84b",
+    "url": "/static/js/main.3d0ba585.chunk.js"
+  },
+  {
+    "revision": "e738b54ffe3b15816cf3",
+    "url": "/static/js/runtime-main.63c1efab.js"
+  }
+]);
